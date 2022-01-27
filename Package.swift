0f3eca376c0c044ce7d60c1cc55ef965d49422b6
deepak-maheshwari-deltatre <90626790+deepak-maheshwari-deltatre@users.noreply.github.com>
2022-01-27 13:59:45 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,11 +19,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "EmitterKit",
-            dependencies: [],
-            path: "Sources"),
-        .testTarget(
-            name: "EmitterKitSpecs",
-            dependencies: ["EmitterKit"],
-            path: "Tests"),
+            dependencies: []),
     ]
 )
